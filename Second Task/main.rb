2c52cd08f2a799e8929afe9f13333f0eae5d7661
Yaroslav Shin <68220034+SitNis@users.noreply.github.com>
2021-11-17 23:45:25 +0300

    Add files via upload

---
+++ Second Task/main.rb
@@ -0,0 +1,241 @@
+require_relative 'station'
+require_relative 'route'
+require_relative 'train'
+require_relative 'train_cargo'
+require_relative 'train_passenger'
+require_relative 'wagon'
+require_relative 'wagon_cargo'
+require_relative 'wagon_passenger'
+
+
+class Main
+  attr_reader :stations, :trains, :routes, :wagons
+  def initialize
+    @stations = []
+    @trains = []
+    @routes = []
+    @wagons = []
+  end
+
+  def menu
+    puts "\n
+      1 - чтобы создать новый объект\n
+      2 - чтобы взаимодействовать с объектом\n
+      3 - чтобы посмотреть список объектов\n
+      0 - чтобы закончить работу\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      create_menu
+    when "2"
+      actions_with_objects
+    when "3"
+      list_menu
+    end
+  end
+
+  private
+
+  def new_station
+    puts "Введи название станции"
+    station_name = gets.chomp()
+    if station_name.length != 0
+      station = Station.new(station_name)
+      @stations.push(station)
+    end
+    menu
+  end
+
+  def new_train
+    puts "Введи номер поезда и тип(Cargo или Passenger)"
+    train_serial_number = gets.chomp()
+    train_type = gets.chomp()
+    if train_type == "Cargo"
+      new_train = Train_cargo.new(train_serial_number)
+      @trains.push(new_train)
+      menu
+    elsif train_type == "Passenger"
+      new_train = Train_passenger.new(train_serial_number)
+      @trains.push(new_train)
+      menu
+    end
+  end
+
+  def new_route
+    puts "Введи название начальной и конечной станций из списка\n
+      #{self.stations}"
+    first_station_name = gets.chomp()
+    last_station_name = gets.chomp()
+    route = Route.new(self.stations.select{|station| station.name == first_station_name}, self.stations.select{|station| station.name == last_station_name})
+    @routes.push(route)
+    menu
+  end
+
+  def new_wagon
+    puts "Введи тип вагона(Cargo или Passenger)"
+    wagon_type = gets.chomp()
+    if wagon_type == "Cargo"
+      new_wagon = Wagon_cargo.new()
+    elsif wagon_type == "Passenger"
+      new_wagon = Wagon_passenger.new()
+    end
+    @wagons.push(new_wagon)
+    menu       
+  end
+
+  def create_menu
+    puts "Что создать?\n
+      1 - станцию\n
+      2 - поезд\n
+      3 - маршрут\n
+      4 - вагон\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      new_station
+    when "2"
+      new_train
+    when "3"
+      new_route
+    when "4"
+      new_wagon            
+    end
+  end
+
+  def hook_wagon(train_id)
+    puts "Какой вагон прикрепить?(Введи порядковый номер до #{self.wagons.length})\n
+      #{self.wagons}"
+    wagon_id = gets.chomp().to_i
+    train_id = train_id
+    self.trains[train_id-1].add_wagon(self.wagons[wagon_id-1])
+    menu
+  end
+
+  def unhook_wagon(train_id)
+    puts "Какой вагон отцепить?(Введи порядковый номер до #{self.trains[train_id-1].wagons.length})\n
+      #{self.trains[train_id-1].wagons}"
+    wagon_id = gets.chomp().to_i
+    train_id = train_id
+    self.trains[train_id-1].delete_wagon(self.trains[train_id-1].wagons[wagon_id-1])
+    menu
+  end
+
+  def route_for_train(train_id)
+    puts "Какой маршрут назначить?(Введи порядковый номер до #{self.routes.length})\n
+      #{self.routes}"
+    route_id = gets.chomp().to_i
+    train_id = train_id
+    self.trains[train_id-1].get_route(self.routes[route_id-1])
+    menu
+  end
+
+  def move_train(train_id)
+    puts "Поехать вперед(1) или назад(2)?"
+    move = gets.chomp()
+    train_id = train_id
+    if move == "1"
+      self.trains[train_id-1].move_forward
+    elsif move == "2"
+      self.trains[train_id-1].move_back
+    end
+    menu
+  end
+
+  def train_menu
+    puts "С каким поездом взаимодействовать?(Введи порядковый номер до #{self.trains.length})\n
+      #{self.trains}"
+    train_id = gets.chomp().to_i
+    puts "\n
+      1 - прикрепить вагон\n
+      2 - открепить вагон\n
+      3 - назначить маршрут\n
+      4 - перемещаться\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      hook_wagon(train_id)
+    when "2"
+      unhook_wagon(train_id)
+    when "3"
+      route_for_train(train_id)
+    when "4"
+      move_train(train_id)
+    end
+  end
+
+  def new_station_in_route(route_id)
+    puts "Какую станцию добавить?\n
+      #{self.stations}"
+    puts "Введи имя станции"
+    station_name = gets.chomp()
+    route_id = route_id
+    self.routes[route_id-1].add_internediate_station(self.stations.select{|station| station.name == station_name})
+    menu
+  end
+
+  def delete_station_in_route(route_id)
+    puts "Какую станцию убрать с маршрута?(Введи имя станции)\n
+      #{self.routes[route_id-1].stations}"
+    station_name = gets.chomp()
+    route_id = route_id
+    self.routes[route_id-1].delete_internediate_station(self.stations.select{|station| station.name == station_name})
+    menu
+  end
+
+  def route_menu
+    puts "С каким маршрутом взаимодействовать?(Порядковый номер до #{self.routes.length}\n
+      #{self.routes})"
+    route_id = gets.chomp().to_i
+    puts "\n
+      1 - добавить станцию\n
+      2 - убрать станцию\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      new_station_in_route(route_id)
+    when "2"
+      delete_station_in_route(route_id)
+    end
+  end
+
+  def actions_with_objects
+    puts "\n
+      1 - взаимодействовать с поездом\n
+      2 - взаимодействовать с маршрутом\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      train_menu
+    when "2"
+      route_menu
+    end
+  end
+
+  def stations_list
+    puts self.stations
+    menu
+  end
+
+  def list_menu
+    puts "\n
+      1 - список станций\n
+      2 - список поездов на станции\n"
+    button = gets.chomp()
+    case button
+    when "1"
+      stations_list
+    when "2"
+      trains_on_station_list
+    end
+  end
+
+  def trains_on_station_list
+    puts "Введи название станции \n
+      #{self.stations}"
+    station_name = gets.chomp()
+    station = self.stations.select{ |station| station.name == station_name }
+    puts station[0].trains.name
+    menu
+  end
+
+end
---
+++ Second Task/wagon_cargo.rb
@@ -0,0 +1,7 @@
+class Wagon_cargo < Wagon
+
+  def initialize(type = "Cargo")
+  	super
+  end
+ 
+end
